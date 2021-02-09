4e31eb0e72e7a145d4cf65f11ed3f3930b0253a1
U8336Tea <gogglinmike@gmail.com>
2021-02-08 22:33:50 -0800

    Allow people to choose vocabulary on launch

--- src/commands/slash/StartRupertCommand.ts
+++ src/commands/slash/StartRupertCommand.ts
@@ -1,17 +1,26 @@
 import type { TextChannel } from "discord.js";
-import { SlashCommand } from "slash-create";
+import { CommandOptionType, SlashCommand } from "slash-create";
 import type { SlashCreator, CommandContext } from "slash-create";
 
-import { findConfig, hasPermission, startResponder } from "../common.js";
+import { findConfig, hasPermission, startResponder, vocabValid } from "../common.js";
 import { rand } from "../../common.js";
 
 export default class StartRupertCommand extends SlashCommand {
     private readonly response = "https://tenor.com/view/mr-krabs-singing-spongebob-christmas-mr-krabs-gif-19652806";
+    private readonly vocabularyKey = "name";
 
     constructor(creator: SlashCreator) {
         super(creator, {
             name: "crapsing", // May change often.
-            description: "Hear Mr. Crap sing! 🤗"
+            description: "Hear Mr. Crap sing! 🤗",
+            options: [
+                {
+                    type: CommandOptionType.STRING,
+                    name: "name",
+                    description: "The name of the person Mr. Crap sings to! 🥰",
+                    required: false
+                }
+            ]
         });
 
         this.filePath = __filename;
@@ -36,6 +45,19 @@ export default class StartRupertCommand extends SlashCommand {
         ctx.send("startig ruppert", {ephemeral: true});
         if (rand(64) == 0) ctx.send(this.response);
 
-        startResponder(findConfig(ctx.guildID, ctx.channelID), ctx.channelID);
+        let config = findConfig(ctx.guildID, ctx.channelID);
+        const vocabulary = ctx.options[this.vocabularyKey].toString();
+        if (vocabulary) {
+            if (!vocabValid(vocabulary)) {
+                const member = ctx.member.user;
+                console.log(`Possible directory traversal attempt from ${member.username}#${member.discriminator}`);
+                console.log(`Path: ${vocabulary}`);
+                return;
+            }
+
+            config.responderName = vocabulary;
+        }
+
+        startResponder(config, ctx.channelID);
     }
 }
