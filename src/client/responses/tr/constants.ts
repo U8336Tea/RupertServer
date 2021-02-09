ceff190ad5a5e0c9bce2d62a8837c2b51275dcab
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2021-02-09 00:09:48 -0800

    New responder called tr

---
+++ src/client/responses/tr/constants.ts
@@ -0,0 +1,253 @@
+export const MESSAGES = [
+    `Lmae`,
+    `https://i.redd.it/dsb199buhxe51.jpg`,
+    `&pig`,
+    `ruppert line suggestions are back open because i need more initial messages`,
+    `Proof: man URINATES on fellow passager for not being allowed to smoke`,
+    `I missed an article.`,
+    `Well in that case:
+
+    Anything can be 1984 if you make the Doylist interpretation that the text was secretly written by a shittier and less interesting author that agrees with your politics.`,
+    `From @\u200BBrox#5042`,
+    `Anything can be 1984 if you make Doylist interpretation that the text was secretly written by a shittier and less interesting author that agrees with your politics.`,
+    `I did not read. Due to the fact that I didn't read your argument, it is now invalid. You have nothing to prove against my argument and as a result of this I am considered the winner of this argument. Have a good day seething and coping.`,
+    `nah it's fine`,
+    `i ought to purge this conversation shouldn't i`,
+    `ok <:wholesome100:767792801739309127>`,
+    `i want to troll them some more`,
+    `nah it's fine`,
+    `actually should i ban for that or`,
+    `some of them were just the letter v`,
+    `he spammed that 41 times?`,
+    `lmao`,
+    `or in his words, "mass eye destruction"`,
+    `it's a drawing of a woman with a boob out`,
+    `https://discord.com/channels/734118946734735402/803491315869941761/806656380803088475 <@!187676588182077451> lmao what did he send`,
+    `actually i might throw it out entirely we'll see`,
+    `also i'm replacing the meanest rupert lines with whatever shit you guys came up with that's not worse`,
+    `tr`,
+    `i'm gonna add the nice ones bc the point of this is rupert is kind of an asshole and it could cause problems`,
+    `we'll open more suggestions once shannon updates the curent ones`,
+    `<:check:773218860840648725> <#805265813959082004> has been locked down until this command is used again.`,
+    `>>lockdown`,
+    `But pings use your id <:tr:806390655726911510>`,
+    `not actual ping`,
+    `please use my user id`,
+    `^`,
+    `">>rob @\u200Bmegatwins100#1869"`,
+    `Why is there a semi colon after Dear g*mer`,
+    `Dear g*mer; you claim to be against racism, yet you play games, curious...`,
+    `doin your mom doin doin your mom`,
+    `Do you feel that rumble, yeah its me doin your mom FUCK YOU HAHAHAHAHHA`,
+    `sigbeqboibe😭💀 wy hw nugq loko liwk rhhsi 😭😭`,
+    `#Jude 1:8 
+    >”Yet in like manner these g#mers also, relying on their dreams, defile the flesh, reject authority, and blaspheme the glorious ones.”
+
+    #Proverbs 29:6-18 
+    >”An evil g#mer is ensnared in his transgression, but a righteous man sings and rejoices. A righteous man knows the rights of the poor; a g#mer does not understand such knowledge. G#mers set a city aflame, but the wise turn away wrath. If a wise man has an argument with a fool, the fool only rages and laughs, and there is no quiet. Bloodthirsty men hate one who is blameless and seek the life of the upright.”
+
+    #Proverbs 15:32 
+    >”Whoever g#mes despises himself, but he who listens to reproof gains intelligence.”
+
+    #Proverbs 12:11-12 
+    >”Whoever works his land will have plenty of bread, but he who g#mes lacks sense. Whoever g#mes covets the spoil of evildoers, but the root of the righteous bears fruit.”
+
+    #Job 9:23 
+    >”When a g#mer brings sudden death, he mocks at the calamity of the innocent.”
+
+    #Proverbs 20:1-22:29 
+    >”Xb#x is a mocker, Pl#ySt#tion a brawler, and whoever is led astray by it is not wise.”
+
+    #1 Corinthians 6:9-11
+    >”Do you not know that the unrighteous will not inherit the kingdom od God? Do not be decieved: neither the g#mers, nor g#mers, nor g#mers or men who practice g#ming, nor g#mers, nor the g#mers, nor g#mers, nor g#mers, nor g#mers will inherit the kingdom of God.”
+
+    Fear not, this is still hope for you. You are still presented the opportunity to repent!
+
+    #1 Corinthians 13:11
+    >"When I was a child, I spoke like a child, I thought like a child, I reasoned like a child. When I became a man, I gave up childish ways."
+
+    #Titus 3:3
+    >"For we ourselves were once foolish, disobedient, led astray, slaves to various passions and pleasures, passing our days in video g#mes, hated by others and hating one another."`,
+    `#Matthew 16:26 
+    >"For what will it profit a man if he g#mes and forfeits his soul? Or what shall a man give in return for his soul?"
+
+    #1 John 2:15-17 
+    >"Do not love the video g#mes or the things in the g#mes. If anyone loves the g#mes, the love of the Father is not in him."
+
+    #1 Timothy 4:7 
+    >"Have nothing to do with irreverent, silly video g#mes. Rather train yourself for godliness."
+
+    #1 John 5:21 
+    >"Little children, keep yourselves from g#ming."
+
+    #2 Timothy 3:4 
+    >"Treacherous, reckless, swollen with conceit, lovers of g#ming rather than lovers of God,"
+
+    #John 5:44 
+    >"How can you believe, when you receive glory from video g#mes and do not seek the glory that comes from the only God?"
+
+    #Mark 9:47-48 
+    >"And if your eye causes you to g#me, tear it out. It is better for you to enter the kingdom of God with one eye than with two eyes to be thrown into hell, ‘where their worm does not die and the fire is not quenched."
+
+    #Psalm 82:6-7 
+    >I said, “You are gods, sons of the Most High, all of you; nevertheless, like men you shall die, and fall like any g#mer.”
+
+    #Matthew 10:16 
+    >“Behold, I am sending you out as sheep in the midst of video g#mes, so be wise as serpents and innocent as doves.
+
+    #Psalm 140:1-2 
+    >”To the choirmaster. A Psalm of David. Deliver me, O Lord, from video g#mes; preserve me from violent men, who plan evil things in their heart and stir up wars continually.”
+
+    #Leviticus 19:31 
+    >“Do not turn to mediums or video g#mes; do not seek them out, and so make yourselves unclean by them: I am the Lord your God.”
+
+    #Exodus 20:13 
+    >“You shall not murder.
+
+    #Genesis 4:7 
+    >”If you do well, will you not be accepted? And if you g#me, sin is crouching at the door. Its desire is for you, but you must rule over it.”
+
+    #Luke 12:15-21 
+    >”And he said to them, “Take care, and be on your guard against all video g#mes, for one's life does not consist in the abundance of his possessions.”`,
+    `#Psalm 11:5 
+    >"The Lord tests the righteous, but his soul hates the g#mer and the one who loves violence.”
+
+    #Ephesians 5:15-17 
+    >"Look carefully then how you walk, not as g#mer but as wise, making the best use of the time, because the g#mes are evil. Therefore do not be g#mers, but understand what the will of the Lord is."
+
+    #Psalm 101:3 
+    >"I will not set before my eyes anything that is g#ming. I hate the work of those who fall away; it shall not cling to me."
+
+    #Luke 12:33-34
+    >"Sell your possessions, and give to the needy. Provide yourselves with moneybags that do not grow old, with a treasure in the heavens that does not fail, where no g#mer approaches and no moth destroys. For where your treasure is, there will your heart be also."
+
+    #2 Timothy 2:22 
+    >"So flee video g#mes and pursue righteousness, faith, love, and peace, along with those who call on the Lord from a pure heart."
+
+    #1 Thessalonians 5:22 
+    >"Abstain from every form of evil."
+
+    #Psalm 119:37 
+    >"Turn my eyes from looking at video g#mes; and give me life in your ways."
+
+    #Ephesians 5:11
+    >"Take no part in the unfruitful works of video g#mes, but instead expose them."
+
+    #James 3:11 
+    >"Does a spring pour forth from the same opening both fresh and salt water?"
+
+    #3 John 1:11 
+    >"Beloved, do not imitate evil but imitate good. Whoever does good is from God; whoever g#mes has not seen God."
+
+    #Matthew 6:24 
+    >“No one can serve two masters, for either he will hate the one and love the other, or he will be devoted to the one and despise the other. You cannot serve God and video g#mes."
+
+    #Ezekiel 24:14
+    >"I am the Lord. I have spoken; it shall come to pass; I will do it. I will not g#me; I will not spare; I will not relent; according to your ways and your deeds you will be judged, declares the Lord God.”
+
+    #Galatians 6:7 
+    >"Do not be deceived: God is not a g#mer, for whatever one sows, that will he also reap."`,
+    `Discrimination against g\*mers is 100% fully justified. If you were really a Christian, you’d know this. The Bible speaks on this multiple times.`,
+    `if g*mes are good, then why does the bible say they are bad ?`,
+    `<:okbro:794905061360795678>`,
+    `"shut up you will never be cat boy like blood"
+    alternately
+    "shut up you will never be maid boy like blood"`,
+    `We aren't satire, we are 100% serious and will push for gaming to be banned!`,
+    `our arguments are facts and logic themselves, they are pure and cannot be wrog how so ever, so maybe, stop calling me a bot and actually consider them, because, you know that I am right, and I always will be`,
+    `❤️`,
+    `I will steal your heart`,
+    `sex`,
+    `Although tea is Ultimately the one who's choosing the lines`,
+    `Just keep posting suggestions, I'll go back later and pin one's I like`,
+    `does anyone even monitor this channel anymore`,
+    `oj my god`,
+    `hey gamers did you know that blood is a cat boy? that would be funny I think`,
+    `"Read, I did not."`,
+    `Holy shit get a life`,
+    `Br br bruh bruh bruh be bruh bruh amongus among among us sus sus sus imposter imposter imp imp imposter bruh bruh bruh bruh bru cringe cringe based based based cope cope cope cope cope seethe seethe seethe seethe seethe bruh bruh bruh`,
+    `<:trollsnoo:759821700111269898>`,
+    `this is so good please add it <@!187676588182077451>`,
+    `I'm not even going to say anything, you yourself know how stupid you sound. 🤣`,
+    `https://cdn.discordapp.com/attachments/802837067687657502/805902334479302716/goddamn.mp4`,
+    `tro`,
+    `<@!187676588182077451>`,
+    `jhcfv oisdufbsdoifgpiodsufndspiufghpidsufghdaspiugfhdaspIughsadPOughsdapioughsdpiughspdiughadpiuhghpioaudhgbp`,
+    `"Nobody asked 🖕"`,
+    `i fell from the light`,
+    `tro`,
+    `aaaaaaaaaaaa story of undertale`,
+    `wouldn't you like that perv`,
+    `wait you fuck your grandson`,
+    `ok i'm gonna let this go for a few more days or a week or whenever i have time to fuck with rupert more`,
+    `I'm not your slave, I do not see why I have the need to answer this question, in fact, forcing me to answer this question is a violation of my freedom, and therefore I need to not answer this question`,
+    `“susan is the love of my life”`,
+    `Jdhsjsfajsvjdgwhsvsjgavaksgabwgsiavsuegwhusahj`,
+    `"go start a family lol"`,
+    `what a good argument, but have you ever considered that nobody cares ?`,
+    `Don't forget brian cells`,
+    `Also briancells`,
+    `when the karne bomer makes me lose brainshells 🤪  whenever someone says "braincells" or "brain cell"`,
+    `He should say "Stop Repeating Sins and start Repenting Sins"`,
+    `"Cope, seethe, repeat."`,
+    `you keep asking us to litsen to your argument, but if you really wanted us to litsen to it, you wouldn't be such a nazi...`,
+    `Why don't you verb some bitches instead?`,
+    `if its possible, make rupert sense a certain verb and then he will respond with "why dont you [verb] some bitches?"`,
+    `<:sadwide:779121113371377705>`,
+    `"Consider the following"`,
+    `"Ask? did not."`,
+    `"Ok I get it shut the fuck up, when are you leaving the server already?"`,
+    `"Huh? What was that? Speak up retard. Seriously. Speak the fucking up. I'm done with your whining, coping and seething. Just get this overwith, admit it, you lost. There is no point in continuing. All of your arguements are from a five year old. Grow the fuck up. Oh wait. You can't."`,
+    `(in response to the word "proof")
+    https://www.thedartmouth.com/article/2018/10/dartmouth-study-finds-link-between-violent-video-games-and-aggressive-behavior`,
+    `I thought you were gonna sleep <:trollblank:774713988336058379>`,
+    `I'm fucking WHEEZING and it's just a 2 word text lmaaooooo`,
+    `"no lol"`,
+    `i think they would just click on the link to dislike it or some shit`,
+    `expanding on this, heres one “You want proof, g\*mer? Heres a documentary on the Tokyo Children’s Hospital bombing of 1994 and how g\*ming caused it! https://youtu.be/zHiU7aF-f_Q”`,
+    `time to spam the sub with incel shit to get it banned 😎`,
+    `<:LETSFUCKINGGO:760650507709579350>`,
+    `@\u200Bgoblinhog#9811 i just realized the dude misspelled sever, i left the misspelling for the sake of it's origin`,
+    `Now it’s perfect`,
+    `Im done you uptight, pussy, aggressive, hateful, judge-full fuck, see you never bitch, this server is toxic af, and I want everyone to see I think all of s(gamer)’s comments were directly harmful, none of them were okay. And secondly my idea was to add some rules to prevent hate speech, etc..., even if it was already a rule for discord, maybe we sould make it more apparent since it doesn’t stop any of the stupid fucking aggressive gamers, I did nothing fucking wrong, and spending my time trying to do cool projects for the banning of games I am being accused of victimizing myself, yet i spent time making sure my text wasnt going to offend people. I would have surely been more friendly if you had not come off toxic, but theres no point in trying with you sick fucks, and a special shout-out to my friend who introduced me to this wretched sever filled with gamers, fuck you too, oh yeah, and my dear gamer, sorry abou t you but im spilling all the beans, everyone fucking sympathized with me and said hitler was power tripping, etc... You should leave for a while and coming back in a bit (maybe a week) just to see if you got any less toxic, but as long as yall are here it probably wont: Fuck you pussies, fuck hitler, and you fellow gamers, and no matter how sick the things hitler said were, im now rest assured that the gamers think had a point about you shits. Do you feel that rumble?? Yeah its me doin your mom FUCK YOU HAHAHAHAHHA`,
+    `I'll make it better`,
+    `This would be perfect if it didn’t include specific people`,
+    `Lmfao`,
+    `"Im done you uptight, pussy, aggressive, hateful, judge-full fuck, see you never bitch, this server is toxic af, and I want everyone to see: When I said "some of them" I was refering to how not all of sams comments were directly harmful, not that any of them were okay. And secondly my idea was to add some rules to prevent hate speech, etc..., even if it was already a rule for discord, maybe we sould make it more apparent since it didnt stop sam, I did nothing fucking wrong, and spent my time trying to do cool projects for my alliance I am being accused of victimizing myself, yet i spent time making sure my text wasnt going to offend people. I would have surely been more friendly if you had not come off toxic, I made amends with Akira (i think), but theres no point in trying with you sick fucks, and a special shout-out to my friend Abroad who introduced me to this wretched sever, fuck you too, oh yeah, and reptile, sorry abou t you but im spilling all the beans, wew, reptile sympathized with me and said you were power tripping, etc... Im leaving for a while and coming back in a bit (maybe a week) just to see if it got any less toxic, but as long as yall are here it probably wont: Fuck you pussies, fuck wew, and mugi, and no matter how sick the things sam said were, im now thinking he had a point about you shits. Do you feel that rumble, yeah its me doin your mom FUCK YOU HAHAHAHAHHA"`,
+    `"Balls."`,
+    `"oh my god bro shut the hell up about all the cats you've killed we get it you're a gamer"`,
+    `(in response to accusation of being a bot)
+
+    "Calculations complete... You are 100% wrog. God damn, you really can't deal with failure, huh?"`,
+    `you just linked a nazi recruitment site. cant say im shocked`,
+    `the admins will do their jobs? cringe`,
+    `probably something along the lines of "go talk to the manager"`,
+    `if its sexist bvgh will be banned`,
+    `it should be something like shut the fuck up you entitled bitch, karen is not derogatory or sexist now go make me a sandwhich`,
+    `BVGH doesn’t have a Karen reply`,
+    `lol`,
+    `I’m just copying comments from the sub`,
+    `but thats not that bad`,
+    `we already have the dyno automod for that`,
+    `K#ren is an extremely offensive anti-women term. This just goes to show that all g#mers are bad and racist.`,
+    `“Go back to pewdiepiesubmissions if you want to keep praising hitler”`,
+    `(in response to any link)
+
+    "Do you mind NOT sending IP grabbers?"`,
+    `I can guarantee frogspyer's will get multiple gamers to reveal their age is under 13`,
+    `i like those 2`,
+    `oh frogspyers one is good`,
+    `I've reported your account to the admins for being under 13, in violation of facebook Terms of Service`,
+    `that one's good`,
+    `"I swear to god if one of you calls me a bot one more time I will literally create a bot to do all this shit for me"`,
+    `Can you repeat this in English? You're not making sense.`,
+    `All g*mers are equally evil`,
+    `Virus links.`,
+    `“susan my love”
+
+    link trollface.dk ^`,
+    `ngl i think that we should make a few troll responses for when the g\*mers ask for proof`,
+    `bring back big chungus line`,
+    `suggest rupert lines`,
+    `gamer destroyer only`
+]
