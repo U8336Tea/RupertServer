665ff67ddc6588c0f0960d35bc3fd82d9a638eba
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2021-02-09 23:56:11 -0800

    Add shutdown command

---
+++ src/commands/dm/shutdown.ts
@@ -0,0 +1,24 @@
+import type { Message } from "discord.js";
+
+function destroy(id: string): Boolean {
+    const responder = global.responders.get(id);
+    if (!responder) return false;
+    responder.destroy();
+    global.responders.delete(id);
+}
+
+export default function(message: Message, command: string[]) {
+    if (command.length < 2) {
+        for (const id of global.responders.keys()) {
+            destroy(id);
+            message.reply("All Ruperts stopped.");
+        }
+    } else {
+        if (!destroy(command[1])) {
+            message.reply("Rupert not running");
+            return;
+        }
+
+        message.reply("Rupert stopped.");
+    }
+}
