45e382b2fa519991c84e9f4e2fa4b687cc587193
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2021-02-09 22:28:23 -0800

    Add start command to DM

---
+++ src/commands/dm/start.ts
@@ -0,0 +1,44 @@
+import { Channel, TextChannel, Message } from "discord.js";
+
+import { pathSafe, findConfig, startResponder } from "../common.js";
+
+export default function(message: Message, command: string[]) {
+    const length = command.length;
+    if (length < 2) return;
+    
+    const discord = global.discord;
+    let channel: Channel;
+
+    if (!(channel = discord.channels.resolve(command[1]))) {
+        message.reply("This isn't a channel in a server I'm in.");
+        return;
+    };
+
+    if (!(channel instanceof TextChannel)) {
+        message.reply("This isn't a text channel.");
+        return;
+    }
+
+    const perms = channel.permissionsFor(global.discord.user);
+    if (!(perms.has("VIEW_CHANNEL") && perms.has("SEND_MESSAGES"))) {
+        message.reply("rupper can't talk in this channel");
+        return;
+    }
+
+    let config = findConfig(channel.guild.id, channel.id);
+    if (length >= 3) {
+        const vocabulary = command[2];
+        if (!pathSafe(vocabulary)) {
+            const member = message.author;
+            console.log(`Possible directory traversal attempt from ${member.username}#${member.discriminator}`);
+            console.log(`Path: ${vocabulary}`);
+            message.reply("Invalid vocabulary.");
+            return;
+        }
+
+        config.vocabulary = vocabulary;
+    }
+
+    message.reply("Starting.");
+    startResponder(config, channel.id);
+}
