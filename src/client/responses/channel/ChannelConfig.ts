4488d4793bf14fb6d8185731133525c379b7fc21
U8336Tea <gogglinmike@gmail.com>
2021-02-13 12:18:06 -0800

    Export parseRules for ease of use.
    Allow blacklisting in ChannelConfig

--- src/client/responses/channel/ChannelConfig.ts
+++ src/client/responses/channel/ChannelConfig.ts
@@ -1,13 +1,23 @@
+import { Rule } from "../../../Rule.js";
+import { parseRules } from "../../../config.js";
+
 export function parseConfig(data: object): ChannelConfig {
     if (!data["channelID"] || !data["earliest"]) return null;
 
+    const blacklist = data["blacklist"] ?? [];
+    if (!blacklist) return null;
+
+    const rules: Rule[] = parseRules(blacklist);
+
     return {
         channelID: data["channelID"],
-        earliest: new Date(data["earliest"])
+        earliest: new Date(data["earliest"]),
+        blacklist: rules
     };
 }
 
 export interface ChannelConfig {
     channelID: string;
-    earliest: Date
+    earliest: Date;
+    blacklist: Rule[];
 }
