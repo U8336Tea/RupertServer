7f478077ae4423960c4926f3b91b6364a7a61863
U8336Tea <gogglinmike@gmail.com>
2021-02-25 22:54:36 -0800

    For some reason, GUILD_CREATE isn't always fired.
    New ready event handler to handle that case

--- src/roleCache.ts
+++ src/roleCache.ts
@@ -2,11 +2,25 @@ import { Collection } from "discord.js";
 
 global.roleCache = new Collection();
 
+interface IDObject {
+    id: string;
+}
+
+function registerRoles(roles: Iterable<IDObject>, guild: IDObject) {
+    for (const role of roles) {
+        global.roleCache.set(role.id, guild.id);
+    }
+}
+
 export function setEvents() {
-    global.discord.ws.on("GUILD_CREATE", data => {
-        for (const role of data.roles) {
-            global.roleCache.set(role.id, data.id);
+    global.discord.on("ready", () => {
+        for (const guild of global.discord.guilds.cache.values()) {
+            registerRoles(guild.roles.cache.values(), guild);
         }
+    });
+
+    global.discord.ws.on("GUILD_CREATE", data => {
+        registerRoles(data.roles, data);
     });
 
     global.discord.on("roleCreate", role => {
